01782b148c22ab01dd85f490111788cca9707c8d
Finite <remieluri@gmail.com>
2021-11-25 21:24:38 -0500

    Added name expressions

---
+++ Dyson/CodeAnalysis/Syntax/Expressions/NameExpressionSyntax.cs
@@ -0,0 +1,20 @@
+﻿namespace Dyson.CodeAnalysis.Syntax.Expressions
+{
+    internal sealed class NameExpressionSyntax
+        : ExpressionSyntax
+    {
+        public NameExpressionSyntax(SyntaxToken identifierToken)
+        {
+            IdentifierToken = identifierToken;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.NameExpression;
+
+        public SyntaxToken IdentifierToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return IdentifierToken;
+        }
+    }
+}
